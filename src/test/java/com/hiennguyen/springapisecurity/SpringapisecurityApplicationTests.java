6f8ac7f1a54ab8d71f366ec3d0203f2d5eb0d63c
Hien Nguyen Dev <nguyenhien.neu.vn@gmail.com>
2024-10-15 18:50:27 +0700

    Step 1: Create Base Project

---
+++ src/test/java/com/hiennguyen/springapisecurity/SpringapisecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiennguyen.springapisecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringapisecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
